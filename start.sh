e89dd22c206b5f9a8f9626c0baa81fe864cde6b0
Jhwyz <167704693+Jhwyz@users.noreply.github.com>
2025-10-23 14:00:10 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# 创建虚拟环境
+python3 -m venv .venv
 
-# 安装依赖
+# 激活虚拟环境
+source .venv/bin/activate
+
+# 更新 pip 并安装依赖
 pip install --upgrade pip
 pip install -r requirements.txt
 
 # 启动机器人
-python3 bot.py
+python bot.py
