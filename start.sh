52d4b26ab055eea06e8010ad586b9a2ab059adf1
Jhwyz <167704693+Jhwyz@users.noreply.github.com>
2025-10-23 11:41:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-export APP_URL="https://jhwlkjjz.onrender.com"
-export PORT=10000
-
-uvicorn main:app --host 0.0.0.0 --port $PORT
+#!/usr/bin/env bash
+# start.sh
+export PORT=${PORT:-10000}
+exec uvicorn main:app --host 0.0.0.0 --port $PORT
